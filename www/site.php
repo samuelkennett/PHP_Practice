44b0d39d49111b3589df872b7e749b44ce80b016
samuelkennett <40373833+samuelkennett@users.noreply.github.com>
2021-02-24 14:36:16 -0500

    POST request practice
    added a simple password submission for getting the hang of post requests. Also cleaned up some of the code to look a little better and added comments

--- www/site.php
+++ www/site.php
@@ -5,42 +5,60 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="style.css">
-        <title> Number Adder</title>
+        <link rel="icon" type="image/png" href="php-logo.png">
+        <title> PHP Practice</title>
     </head>
 
     <body>
-        <h1 class = "title"> Number Adder</h1>
-
+        <h1 class = "title"> PHP Practice</h1>
         <p class = "date">Today's date is : <?php echo date('F j, Y') ?></p> <!--  pulling the date from the server using PHP-->
 
-        <form action = "site.php" method="get" class = "num-adder-container">
-
+        <!-- GET Testing -->
+        <form action = "site.php" method="get" class = "container">
             <!--PHP function to generate random numbers and assign to vars-->
             <?php 
-
                 $num1 = rand(1,20);
                 $num2 = rand(1,20);
-
             ?>
-
             First number:
-            <input type="number" name = "firstNum" value = "<?php echo $num1?>" >
-
+            <input type="number" name = "firstNum" value = "<?php echo $num1?>" > <!-- echos num1 to the input box-->
             <br>
             <br>
-
             Second Number:
-           <input type="number" name="secNum" value = "<?php echo $num2?>">
-
+           <input type="number" name="secNum" value = "<?php echo $num2?>"> <!-- echos num2 to the input box-->
           <input type="submit"> 
         </form>
 
         <br>
+ 
+        <p style="font-family:Arial, Helvetica, sans-serif; color:crimson; font-size:x-large">Result:
+        <?php 
+            //Uses the GET command to retrieve the numbers from the input boxes and sums them
+            //NOTE: Uses fallback zero because once the next form is submitted an undefined array key warning pops up
+            //      this way the result defaults back to zero since there is no functionality for saving $num1 and $num2 after the form is submitted
+            $num1 = $_GET["firstNum"] ?? 0;
+            $num2 =  $_GET["secNum"] ?? 0;
 
-        <h2>Result:</h2>  <?php 
-            echo $_GET["firstNum"] + $_GET["secNum"];
-            
+            echo  $num1 + $num2;
          ?> 
+         </p>
+
+        <br>
+        <br>
+
+         <!-- POST Testing -->
+
+         <form class = "container" action = "site.php" method = "post">
+            <h3>Enter a Password</h3>
+            <div>Password: <input type="password" class="password-box" name="password"> </div>
+            <p>Entered:
+            <?php 
+                echo $_POST["password"] ?? ""; //handles warning undefined array key will fallback ""
+             ?> 
+            </p> 
+            
+            
+         </form>
 
     </body>
 
